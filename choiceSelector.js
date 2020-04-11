717b5449b8438d714597b016edbcb403ff48f56a
abelcgim <63255806+abelcgim@users.noreply.github.com>
2020-04-11 14:04:11 +0200

    fix bugs, nuevas interacciones hora

--- choiceSelector.js
+++ choiceSelector.js
@@ -1,9 +1,11 @@
-var data = '{"HABITACIÓN":{"initText":"Te tumbas en la cama de tu habitación.","choice1":{"button":"MIRAR MÓVIL","time_incremental":25,"text":"Miras el móvil durante 25 minutos.<p>Te han enviado el video de Resistiré por varios grupos de WhatsApp.</p>"},"choice2":{"button":"MIRAR PARED","time_incremental":15,"text":"Inspeccionas la pared durante 15 minutos, es una pared blanca ordinaria."}},"VENTANA":{"initText":"Tu piso de mierda no tiene terraza, te asomas a la ventana de tu bajo.","choice1":{"button":"APLAUDIR","text":"El cabrón del balcón de enfrente se te ha vuelto a adelantar, aplaudes durante 5 minutos junto al vecindario."}},"BAÑO":{"initText":"Intentas abrir la puerta pero está cerrada. Al inspeccionarla escuchas ruidos que provienen de dentro.","choice1":{"button":"INVESTIGAR RUIDOS","text":"Efectivamente, hay alquien cagando dentro."},"choice2":{"button":"IGNORAR RUIDOS","text":"Decides que lo que este pasando hay dentro no es tan interesante."}},"SALÓN":{"initText":"Estás en tu salón. Entre tus posesiones destaca una TV y una cortina.","choice1":{"button":"VER TV","time_incremental":5,"text":"Ves la tele durante 5 minutos.<p>Esta hablando Pedro Sanchéz, parece que extienden la cuarentena otras dos semanas.</p>"},"choice2":{"button":"ABRIR CORTINA","text":"Anda, si estaba aquí la perra."}},"COCINA":{"initText":"Estás en tu cocina. Te quedan 10 latas de cerveza, quizás deberias racionarlas.","choice1":{"button":"BEBER CERVEZA","text":"Has bebido X, te quedan Y latas."}},"PASILLO":{"initText":"¿Quien necesita salir a la calle teniendo un pasillo de 2 metros de largo? Te preparas para ejercitar tu cuerpo.","choice1":{"time_incremental":5,"button":"CAMINAR","text":"Caminas de un lado a otro del pasillo. Das 50 vueltas en 5 minutos."},"choice2":{"button":"HACER FLEXIONES","text":"Intentas hacer una flexión, pero desistes."}},"MERCADONA":{"initText":"Lo sentimos, el contenido descargable RETO DE COMPRA EN MERCADONA no está actualmente disponible."},"CALLE":{"initText":"¿Estás seguro de querer salir a la calle? No tienes necesidad.","choice1":{"button":"SALIR","text":""},"choice2":{"button":"NO SALIR","text":"Decides no ser un subnormal que sale a la calle sin motivo."}},"ENCUENTRO CON POLICÍA":{"initText":"Sales a la calle a estirar las piernas, al doblar la esquina te encuentras un policía. ¿Adonde va usted señor?","choice1":{"button":"A PASEAR A LA PERRA","text":""}}}';
+var data = '{"HABITACIÓN":{"initText":"Te tumbas en la cama de tu habitación.","choice1":{"button":"MIRAR MÓVIL","time_incremental":25,"text1":"Miras el móvil durante 25 minutos.<p>Notificaciones WhatsApp: Videos de Resistiré por varios grupos.</p>","text2":"Miras el móvil durante 25 minutos.<p>Notificaciones WhatsApp: Videos con los africanos del ataúd.</p>","text3":"Miras el móvil durante 25 minutos.<p>Notificaciones WhatsApp: Memes varios del coronavirus.</p>","text4":"Miras el móvil durante 25 minutos.<p>Notificaciones WhatsApp: Videos de gente pillada saltándose la cuarentena.</p>"},"choice2":{"button":"MIRAR PARED","time_incremental":15,"text":"Inspeccionas la pared durante 15 minutos, es una pared blanca ordinaria."}},"VENTANA":{"initText":"Tu piso de mierda no tiene terraza, te asomas a la ventana de tu bajo.","choice1":{"button":"APLAUDIR","text":"El cabrón del balcón de enfrente se te ha vuelto a adelantar, aplaudes durante 5 minutos junto al vecindario."}},"BAÑO":{"initText":"Intentas abrir la puerta pero está cerrada. Al inspeccionarla escuchas ruidos que provienen de dentro.","choice1":{"button":"INVESTIGAR RUIDOS","text":"Efectivamente, hay alquien cagando dentro."},"choice2":{"button":"IGNORAR RUIDOS","text":"Decides que lo que este pasando hay dentro no es tan interesante."}},"SALÓN":{"initText":"Estás en tu salón. Entre tus posesiones destaca una TV y una cortina.","choice1":{"button":"VER TV","time_incremental":10,"text":"Ves la tele durante 10 minutos.<p>Esta hablando Pedro Sanchéz, parece que extienden la cuarentena otras dos semanas.</p>"},"choice2":{"button":"ABRIR CORTINA","text":"Anda, si estaba aquí la perra."}},"COCINA":{"initText":"Estás en tu cocina. Te quedan 10 latas de cerveza, quizás deberias racionarlas.","choice1":{"button":"BEBER CERVEZA","text":"Has bebido X, te quedan Y latas."}},"PASILLO":{"initText":"¿Quien necesita salir a la calle teniendo un pasillo de 2 metros de largo? Te preparas para ejercitar tu cuerpo.","choice1":{"time_incremental":5,"button":"CAMINAR","text":"Caminas de un lado a otro del pasillo. Das 50 vueltas en 5 minutos."},"choice2":{"button":"HACER FLEXIONES","text":"Intentas hacer una flexión, pero desistes."}},"MERCADONA":{"initText":"Lo sentimos, el contenido descargable RETO DE COMPRA EN MERCADONA no está actualmente disponible."},"CALLE":{"initText":"¿Estás seguro de querer salir a la calle? No tienes necesidad.","choice1":{"button":"SALIR","text":""},"choice2":{"button":"NO SALIR","text":"Decides no ser un subnormal que sale a la calle sin motivo."}},"ENCUENTRO CON POLICÍA":{"initText":"Sales a la calle a estirar las piernas, al doblar la esquina te encuentras un policía. ¿Adonde va usted señor?","choice1":{"button":"A PASEAR A LA PERRA","text":""}}}';
 var json = JSON.parse(data);
 var time=new Date(2020,03,12,15,58,0);
 var dogFound=false;
 var infractorHabitual=false;
+var clapping=false;
 var exercise=false;
+var firstNight=false;
 var multas=0;
 var beerleft=10
 var beerdrunk=0
@@ -19,6 +21,25 @@ function reset() {
 
 function replaceAt(string, index, replace) {
   return string.substring(0, index) + replace + string.substring(index + 1);
+}
+
+function addTime(value) {
+	time=new Date(time.getTime() + value * 60000);
+	if (time.getHours()==0 && time.getUTCDate()==14) {
+		document.querySelector(".container").remove();
+      	document.querySelector(".currentTextOver").innerHTML="<p>Tras varios dias sin comer te desmayas. Si hubieras ido al MERCADONA esto no habría pasado...</p><p>¿Dices que no estaba la opción disponible? Mala suerte.</p><p>LOGRO CONSEGUIDO: DESNUTRIDO</p>";
+      	modifyAchievements("DESNUTRIDO");      	    
+      	document.querySelector(".over").style.display = "block";	
+	}
+	else if (time.getHours()==0) {
+		document.querySelector(".currentText").innerHTML="Tras un intenso día de cuarentena caes dormido. Te despiertas a la mañana siguiente."
+		if (!firstNight) {
+		firstNight=true;
+		document.querySelector(".currentText").innerHTML+="<p>LOGRO CONSEGUIDO: RUTINA</p>";
+		modifyAchievements("RUTINA");   
+		}	
+		time=new Date(time.getTime() + 600 * 60000);
+	}
 }
 
 function modifyInventory(value) {
@@ -65,25 +86,23 @@ function choiceSelector(value) {
   document.querySelector("#text").classList.remove("largerText");
   var location=document.querySelector(".current-location").innerText;
   var currentText=document.querySelector(".currentText");
-  var textToShow;
+  currentText.innerHTML=json[location][value].text;
   if (location=="VENTANA" && value=="choice1") {
   	  var timeHourMinute=time.getHours()+":"+String(time.getMinutes()).padStart(2, "0");
   	  if (timeHourMinute=="19:58") {
-  	  document.querySelector("#text").classList.add("largerText");
-  	  modifyAchievements("SOLIDARIDAD");
-  	  textToShow="El cabrón del balcón de enfrente se te ha vuelto a adelantar, aplaudes durante 5 minutos junto al vecindario. <p>LOGRO CONSEGUIDO: SOLIDARIDAD</p>";
-  	  time=new Date(time.getTime() + 5 * 60000);
-  	  document.querySelector(".ui-time-place span").innerText=time.toLocaleString();
-  	  } else textToShow="...que son las "+timeHourMinute+ ", no las 19:58.";
-  } else textToShow=json[location][value].text;
-  var timeIncremental=json[location][value].time_incremental
-  if (json[location][value].time_incremental!=undefined) {
-  	 time=new Date(time.getTime() + timeIncremental * 60000);
-  	 document.querySelector(".ui-time-place span").innerText=time.toLocaleString();
-  }  
-  currentText.innerHTML=textToShow;
-    if (location=="SALÓN" && value=="choice1") {
-  	  json[location][value].text="Ves la tele durante 5 minutos.<p>No parece que el discurso de Pedro Sanchéz vaya a acabar pronto.</p>";
+  	    document.querySelector("#text").classList.add("largerText");  	 
+  	    currentText.innerHTML="El cabrón del balcón de enfrente se te ha vuelto a adelantar, aplaudes durante 5 minutos junto al vecindario.";
+  	    if (!clapping) {
+  	  	    clapping=true;
+  	  	    modifyAchievements("SOLIDARIDAD");
+  	  	    currentText.innerHTML+="<p>LOGRO CONSEGUIDO: SOLIDARIDAD</p>";
+  	    }
+  	    addTime(5);
+  	    document.querySelector(".ui-time-place span").innerText=time.toLocaleString();
+  	  } else currentText.innerHTML="...que son las "+timeHourMinute+ ", no las 19:58.";
+  }
+  if (location=="SALÓN" && value=="choice1") {
+  	  json[location][value].text="Ves la tele durante 10 minutos.<p>No parece que el discurso de Pedro Sanchéz vaya a acabar pronto.</p>";
   }
   if (location=="SALÓN" && value=="choice2" && !dogFound) {
   	 currentText.innerHTML+="<p>PERRA añadida al INVENTARIO</p>";
@@ -100,16 +119,20 @@ function choiceSelector(value) {
   	  if (beerleft==1) {
   	  	  currentText.innerHTML="No se que pretendías conseguir pero te has quedado sin cerveza.";
   	  	  json[location].initText="Hay latas de cerveza vacías desperdigadas por la mesa.";
-  	  	  delete json[location]['choice1'];
+  	  	  delete json[location][value];
   	  	  document.querySelector("#choice1").style.display = "none";
-  	  	  console.log(json[location]);
   	  }
   	  else {
   	    beerleft--
   	    beerdrunk++
+  	    json[location].initText="Estás en tu cocina. Te quedan "+beerleft+" latas de cerveza, quizás deberias racionarlas."
   	    currentText.innerHTML=currentText.innerHTML.replace("X", beerdrunk);
   	    currentText.innerHTML=currentText.innerHTML.replace("Y", beerleft);
       }
+  }
+  if (location=="HABITACIÓN" && value=="choice1") {
+  	  var randomText="text"+String(Math.floor(Math.random() * 4) + 1);
+  	  currentText.innerHTML=json[location][value][randomText];
   }
   if (location=="HABITACIÓN" && value=="choice2") {
   	  json[location][value].text="Inspeccionas la pared durante otros 15 minutos, sigue siendo una pared blanca ordinaria.";
@@ -127,9 +150,9 @@ function choiceSelector(value) {
       multas++
       if (multas>9) {
       	    document.querySelector(".container").remove();
-      	    document.querySelector(".currentTextJail").innerHTML="<p>Tras multarte 10 veces el policía se harta y te manda a la cárcel. Deberías haberlo visto venir...</p><p>LOGRO CONSEGUIDO: CONDENADO</p>";
+      	    document.querySelector(".currentTextOver").innerHTML="<p>Tras multarte 10 veces el policía se harta y te manda a la cárcel. Deberías haberlo visto venir...</p><p>LOGRO CONSEGUIDO: CONDENADO</p>";
       	    modifyAchievements("CONDENADO");      	    
-      	    document.querySelector(".jail").style.display = "block";
+      	    document.querySelector(".over").style.display = "block";
       	    
       }	  
       else if (multas>1 && !infractorHabitual) {
@@ -142,4 +165,10 @@ function choiceSelector(value) {
         document.querySelector("#choice1").style.display = "none";
       }
   }
+  if (json[location][value]!=undefined && json[location][value].time_incremental!=undefined) {
+  	 var timeIncremental=json[location][value].time_incremental
+  	 addTime(timeIncremental);
+  	 if (document.querySelector(".ui-time-place span")!=undefined)
+  	 document.querySelector(".ui-time-place span").innerText=time.toLocaleString();
+  }  
 }
