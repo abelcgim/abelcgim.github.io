a04b2bb867a8ca2e7546f676540c145b709e4a73
abelcgim <63255806+abelcgim@users.noreply.github.com>
2020-05-02 00:27:07 +0200

    ajustes

--- endOfTime.js
+++ endOfTime.js
@@ -1,10 +1,11 @@
 var data='{"time1":"<p>2019 que tiempos tan felices...quien iba a decir que en 2020 se iba a ir todo a la mierda.</p><p>Mejor no interferir aquí.</p>","time2":"<p>¿Estás preparado para embarcarte en una aventura épica de viajes por el tiempo para finalmente evitar el primer contagio por coronavirus?</p><p>Espero que no, porque no vas a poder.</p>","time3":"<p>Lo tienes muy visto, mejor ir al futuro.</p>"}';
 var json = JSON.parse(data);
-window.onload = function(e){  
-  $('audio').mediaelementplayer();
-  setTimeout(function(){
-      document.querySelector("#player").play();
-  }, 500);  
+window.onload = function(e){  	
+	$('audio').mediaelementplayer({
+	    success: function(mediaElement, domObject) {
+	        document.querySelector("#player").play();
+	    }
+	});
 };
 
 
@@ -21,6 +22,7 @@ function timeTravel() {
 	document.querySelector("#playervideo").play();
 	document.querySelector(".final-container-eot").classList.remove("hidden");
 	setTimeout(function(){
+	   document.querySelector(".final-container-eot").remove();
        window.location.href = './simulador-cuarentena-2m.html';
     }, 8000);
 }
