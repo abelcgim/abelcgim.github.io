6c5d79c090900d5df1b02d3aaf271b2bec6fbd09
abelcgim <63255806+abelcgim@users.noreply.github.com>
2023-04-25 18:31:28 +0200

    dezi 2

--- catGirl.js
+++ catGirl.js
@@ -1,5 +1,13 @@
 
-function introMode() {
+function burp1() {
 	document.querySelector(".audio-container audio").pause();
+	document.querySelector(".audio-container audio").src="./dezi.mp3"
+	document.querySelector(".audio-container audio").play();
+}
+
+
+function burp2() {
+	document.querySelector(".audio-container audio").pause();
+	document.querySelector(".audio-container audio").src="./dezi2.mp3"
 	document.querySelector(".audio-container audio").play();
 }
